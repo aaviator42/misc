559d486234f293c5d9fe483c20bda7c72af93e50
Aavi <83829272+aaviator42@users.noreply.github.com>
2022-04-16 18:45:01 +0530

    Add files via upload

--- npp.cmd
+++ npp.cmd
@@ -1,3 +1,17 @@
 @echo off
-set filep=%cd%\\%*
-start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "%filep%"
+set filep=%cd%\%1
+set filep=%filep:"=%
+set filep="%filep%"
+
+if [%1]==[] (
+	goto :eof
+)
+
+if exist %filep% (
+	start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" %filep%
+) else (
+	start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "%1"
+)
+
+
+:eof
